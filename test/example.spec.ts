4fc35f0c0c4af6fa9155df589e90079e8a9f58ee
Gustavo <gustavodetomi@gmail.com>
2026-03-15 22:31:34 -0300

    add list transactions test

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -1,16 +1,18 @@
 import request from "supertest";
-import { afterAll, beforeAll, describe, expect, test } from "vitest";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
 import { app } from "../src/app";
 
-beforeAll(async () => {
-  await app.ready()
-});
 
-afterAll(async () => {  
-  await app.close()
-});
 describe('Transações', () => {
-  test('Usuário cria uma nova transação', async () => {
+  beforeAll(async () => {
+    await app.ready()
+  });
+  
+  afterAll(async () => {  
+    await app.close()
+  });
+
+  it('Usuário cria uma nova transação', async () => {
     const response = await request(app.server)
       .post('/transactions')
       .send({
@@ -18,7 +20,28 @@ describe('Transações', () => {
       amount: 5000,
       type: 'credit'
     });
-
     expect(response.statusCode).toEqual(201);
   });
+
+  it('Usuário lista as transações', async () => {
+    const createTransactionResponse = await request(app.server)
+    .post('/transactions')
+    .send({
+    title: 'Freelance de website',
+    amount: 5000,
+    type: 'credit'
+  });
+  const cookies = createTransactionResponse.get('Set-Cookie');
+
+  const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies);
+    expect(listTransactionsResponse.statusCode).toEqual(200);
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'Freelance de website',
+        amount: 5000,
+      })
+    ]);
+  })
 });
