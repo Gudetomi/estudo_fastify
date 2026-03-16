a167f813d9c2ba4cfef23f38a9f61b681b0d576e
Gustavo <gustavodetomi@gmail.com>
2026-03-15 22:10:12 -0300

    add describe

--- test/example.spec.ts
+++ test/example.spec.ts
@@ -1,5 +1,5 @@
 import request from "supertest";
-import { afterAll, beforeAll, expect, test } from "vitest";
+import { afterAll, beforeAll, describe, expect, test } from "vitest";
 import { app } from "../src/app";
 
 beforeAll(async () => {
@@ -9,17 +9,16 @@ beforeAll(async () => {
 afterAll(async () => {  
   await app.close()
 });
-
-test('Usuário cria uma nova transação', async () => {
-  const response = await request(app.server)
-    .post('/transactions')
-    .send({
+describe('Transações', () => {
+  test('Usuário cria uma nova transação', async () => {
+    const response = await request(app.server)
+      .post('/transactions')
+      .send({
       title: 'Freelance de website',
       amount: 5000,
       type: 'credit'
     });
 
     expect(response.statusCode).toEqual(201);
-
-
+  });
 });
